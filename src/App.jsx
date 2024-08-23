b6f2b3020a49ca0e7ac4e3b596abd540ce1fa795
Munkleson <alexwongsydney@gmail.com>
2024-08-23 21:44:19 +0900

    Initial commit. Removed vite default build for App.jsx and put my own function in there to test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import GeneralInfo from './generalInfo'
+
+function App() {
+
+  return (
+    <>
+      <GeneralInfo />
+    </>
+  )
+}
+
+export default App
---
+++ src/generalInfo.jsx
@@ -0,0 +1,7 @@
+export default function generalInfo() {
+    return <form>
+        <input type="text" placeholder="Name"/>
+        <input type="email" placeholder="email"/>
+        <input type="number" placeholder="Phone Number"/>
+    </form>
+}
