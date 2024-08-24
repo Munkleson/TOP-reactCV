f52df8589504fac7b096ee5db7b64a04ed201233
Munkleson <alexwongsydney@gmail.com>
2024-08-24 13:09:18 +0900

    Made each div be able to be edited and not affected the others, by using state. Not the most efficient layout so far though

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,94 @@ import { GeneralInfo, EducationalExperience, PracticalExperience } from "./formF
 // const cvObject = {};
 
 function App() {
-    const [postedState, setPostedState] = useState(false);
+    const [cvList, setCvList] = useState([]);
+    const [cvListId, setCvListId] = useState(0);
 
     function submitButton(event) {
         event.preventDefault();
-        setPostedState(postedState ? false : true);
         const documentWhole = document.querySelectorAll(".formInput");
-        const cvObject = {};
+        const cvObject = { content: {} };
         documentWhole.forEach((element) => {
-            cvObject[element.getAttribute("placeholder")] = element.value;
+            cvObject.content[element.getAttribute("placeholder")] = element.value;
         });
-        newCv(cvObject);
+        cvObject.Key = cvListId;
+        setCvList((prevCvList) => [...prevCvList, cvObject]);
+        setCvListId(cvListId + 1);
     }
 
     return (
-        <form className="cvForm">
-            <GeneralInfo postedState={postedState} />
-            <EducationalExperience postedState={postedState} />
-            <PracticalExperience postedState={postedState} />
-            <input type="submit" value="Submit" className="cvFormSubmit" onClick={submitButton} />
-        </form>
+        <>
+            <form className="cvForm">
+                <GeneralInfo />
+                <EducationalExperience />
+                <PracticalExperience />
+                <input type="submit" value="Submit" className="cvFormSubmit" onClick={submitButton} />
+            </form>
+            <div className="cvCardHolderDiv">{cvList.length > 0 && <NewCv cvList={cvList} />}</div>
+        </>
     );
 }
 
 export default App;
 
-function newCv(cvObject) {
+function NewCv({ cvList }) {
+    return (
+        <>
+            {cvList.map((element) => {
+                return (
+                    <div className="cvDiv" key={element.Key}>
+                        <CvContent list={element} identifier={element.Key}/>
+                    </div>
+                );
+            })}
+        </>
+    );
+}
 
-    
+function CvContent({ list, identifier }) {
+    const [listContent, setListContent] = useState(list.content);
+    const [editState, setEditState] = useState(false);
+
+    function editObject() {
+        setEditState(true);
+    }
+
+    function submitEdit() {
+        setEditState(false);
+    }
+
+    function changeContent(event) {
+        setListContent(currentContent => ({
+            ...currentContent, 
+            Name: event.target.value
+        }))
+    }
+
+    return (
+        <>
+            {!editState ? (
+                <div>
+                    <p>{listContent.Name}</p>
+                    <p>{identifier}</p>
+                    <button onClick={editObject}>Edit</button>
+                </div>
+            ) : (
+                <div>
+                    <input type="text" value={listContent.Name} onChange={changeContent}/>
+                    <button onClick={submitEdit}>Submit edit</button>
+                </div>
+            )}
+            {!editState ? (
+                <div>
+                    <p>{listContent.Email}</p>
+                    <button onClick={editObject}>Edit</button>
+                </div>
+            ) : (
+                <div>
+                    <input type="text" value={listContent.Email} onChange={changeContent}/>
+                    <button onClick={submitEdit}>Submit edit</button>
+                </div>
+            )}
+        </>
+    );
 }
